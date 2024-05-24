7bbf2777a227bd70ceb890381be4f6b2cc68ca1b
yelsin1204 <heryelsin123@gmail.com>
2024-05-23 23:36:41 -0500

    se agrega proyecto

---
+++ vistas/acerca.php
@@ -0,0 +1,49 @@
+<?php
+//Activamos el almacenamiento en el buffer
+ob_start();
+session_start();
+
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: login.html");
+}
+else
+{
+require 'header.php';
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Acerca de</h1>
+	                        <div class="box-tools pull-right">
+	                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body">
+                    	<h4>Proyecto: </h4> <p>AdVentas 3.0 - Sistema de Ventas, Compras y Almacén</p>
+		             
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+<?php
+require 'footer.php';
+?>
+<?php 
+}
+ob_end_flush();
+?>
+
+
---
+++ vistas/ayuda.php
@@ -0,0 +1,46 @@
+<?php
+//Activamos el almacenamiento en el buffer
+ob_start();
+session_start();
+
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: login.html");
+}
+else
+{
+require 'header.php';
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Ayuda</h1>
+	                        <div class="box-tools pull-right">
+	                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body">
+                  
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+<?php
+require 'footer.php';
+?>
+<?php 
+}
+ob_end_flush();
+?>
